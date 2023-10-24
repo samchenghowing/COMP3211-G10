76e9269147dd8039d6cdf523c6a088b5a17895ea
Carter WU <cwu8079@gmail.com>
2023-10-24 20:11:15 +0800

    chao

--- src/model/Task.java
+++ src/model/Task.java
@@ -3,7 +3,9 @@ package model;
 public class Task extends PIR implements ManagePIR {
 
 	@Override
-	public void create() {
+	public void create() 
+	{
+		System.out.println("I modified here");
 		// TODO Auto-generated method stub
 		
 	}
