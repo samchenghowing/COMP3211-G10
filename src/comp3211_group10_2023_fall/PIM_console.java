7ec2c52e338d5a2e49be6fc2dda23811db1957a7
samchenghowing <samchenghowing@gmail.com>
2023-10-24 13:20:41 +0800

    first commit

---
+++ src/comp3211_group10_2023_fall/PIM_console.java
@@ -0,0 +1,10 @@
+package comp3211_group10_2023_fall;
+
+public class PIM_console {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
